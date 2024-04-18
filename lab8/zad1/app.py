98e3504ff5f65024b795f1a13665d5e2d2570d86
Alicja Chmura <ala.chmura2003@gmail.com>
2024-04-18 20:31:10 +0200

    Zadanie 1

---
+++ lab8/zad1/app.py
@@ -0,0 +1,13 @@
+import os
+from flask import Flask
+
+port = 8080
+
+app = Flask(__name__)
+
+@app.route('/')
+def hello():
+    return "Hello, world!"
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=port)
